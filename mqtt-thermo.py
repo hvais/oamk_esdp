fdc1e395a7a8f069d949faf1ae84b9f3c6e9e900
Henry <henryvaisanen@gmail.com>
2017-12-03 06:36:55 +0200

    added mqtt-thermo.py

---
+++ mqtt-thermo.py
@@ -0,0 +1,63 @@
+import os
+import time
+import sys
+import glob
+import paho.mqtt.client as mqtt
+import json
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+THINGSBOARD_HOST = 'demo.thingsboard.io'
+ACCESS_TOKEN = 'KU5KTpOzqZqOV3Semo4c'
+
+INTERVAL=5
+
+sensor_data = {'temperature': 0}
+
+next_reading = time.time() 
+
+client = mqtt.Client()
+client.username_pw_set(ACCESS_TOKEN)
+client.connect(THINGSBOARD_HOST, 1883, 60)
+client.loop_start()
+
+try:
+    while True:
+        temperature = read_temp()
+        temperature = round(temperature, 2)
+        print(temperature)
+        sensor_data['temperature'] = temperature
+
+        client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+
+        next_reading += INTERVAL
+        sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
+client.disconnect()
